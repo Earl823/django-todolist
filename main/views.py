c37cac7742c424ea77d7b181e5f03e6f4fa0078c
Earl823 <earl.bayron23@gmail.com>
2023-02-25 17:18:32 +0800

    adding database

--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
 # this ia the hompage function
 def index(request):
     td = request.POST['todo']
-    return render(request, 'home.html', {'name': td})
+    insert = Item(text=td, complete=False)
+    #btn = request.POST['remove']
+
+
+    item = Item.objects.all()
+    #Item.objects.filter(id=btn).delete()
+
+    insert.save()
+    
+    return render(request, 'home.html', {'name': item})
 
 
 
